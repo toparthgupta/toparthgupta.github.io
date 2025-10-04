ffe48d2a8ba711ba0bdb5b6eb106a734088edec9
toparthgupta <154114933+toparthgupta@users.noreply.github.com>
2025-10-04 16:24:01 +0530

    Update interest-kit.js

--- interest-kit.js
+++ interest-kit.js
@@ -139,18 +139,6 @@
       try {
         if (global.agentforce_messaging && global.agentforce_messaging.utilAPI && typeof global.agentforce_messaging.utilAPI.setEngagement === 'function' && typeof global.agentforce_messaging.utilAPI.setSessionContext === 'function') {
           global.agentforce_messaging.utilAPI.setEngagement(global.INTEREST_KIT_DATA);
-          global.agentforce_messaging.utilAPI.setSessionContext([
-			    {
-			        "name": "_AgentContext",
-			        "value": {
-			            "valueType": "StructuredValue",
-			            "value": {
-			                "currentPage": "Blueberry",
-			                "Interest": "Pineapple"
-			            }
-			        }
-			    }
-			  ]);
           this._engagementRetryCount = 0; // reset on success
           if (sendSalesforceEvents) {
             this._sendSalesforceInteractions();
